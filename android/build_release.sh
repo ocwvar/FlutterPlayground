935e39bc672b5602e4b32a32833af86a728181a2
妇校长 <ocwvarx2@gmail.com>
2022-04-21 00:45:17 +0800

    Update build_release.sh

--- android/build_release.sh
+++ android/build_release.sh
@@ -1,4 +1,3 @@
-clear
 # get all args from outside first
 password=$1
 aliasName=$2
@@ -7,13 +6,13 @@ encodedSignFileString=$4
 
 # write them into key.properties
 echo "Writing secrets into properties file"
-echo "password=$password" > "key.properties"
-echo "aliasName=$aliasName" >> "key.properties"
-echo "signFilePath=../$signFileName" >> "key.properties"
+echo "password=$password" > key.properties
+echo "aliasName=$aliasName" >> key.properties
+echo "signFilePath=../$signFileName" >> key.properties
 
 echo "Decoding SignFile Base64 and output to local"
-echo "$encodedSignFileString" > "key.temp"
-base64 --decode "key.temp" > "$signFileName"
+echo "$encodedSignFileString" > key.temp
+base64 --decode key.temp > "$signFileName"
 rm -rf ./key.temp
 
 echo "Begin to build release APK"
