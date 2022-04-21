0917d12c45afa0b0963fd99cdbfca71c67d66e60
ocwvar <ocwvarx2@gmail.com>
2022-04-21 11:02:57 +0800

    update script

--- android/build_release.sh
+++ android/build_release.sh
@@ -1,24 +1,3 @@
-# get all args from outside first
-pwd=$1
-aliasName=$2
-signFileName=$3
-encodedSignFileString=$4
-
-# write them into key.properties
-echo "Writing secrets into properties file"
-echo "password=$pwd" > key.properties
-echo "aliasName=$aliasName" >> key.properties
-echo "signFilePath=../$signFileName" >> key.properties
-
 echo "Decoding SignFile Base64 and output to local"
-echo "$encodedSignFileString" > key.temp
-base64 --decode key.temp > "$signFileName"
+base64 --decode key.temp > "sign.key"
 ls -l
-rm -rf key.temp
-
-echo "Begin to build release APK"
-flutter build apk
-
-echo "Delete sign file and properties file"
-rm -rf "$signFileName"
-rm -rf key.properties
