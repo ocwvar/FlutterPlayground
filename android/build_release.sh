44da233e6eebbcbe867ff84debeee51bfa26523d
ocwvar <ocwvarx2@gmail.com>
2022-04-21 11:21:50 +0800

    update script

--- android/build_release.sh
+++ android/build_release.sh
@@ -1,7 +1,16 @@
+# switch to work folder
 cd android/
+
+# all those env params are set in dart.yml
+echo "Writing secrets to file"
+echo password=$PWD >> android/key.properties
+echo aliasName=$ALIAS >> android/key.properties
+echo signFilePath=../$SIGN_FILE_NAME >> android/key.properties
+echo $ENCODED_SIGN_FILE >> android/key.temp
+
 echo "Decoding SignFile Base64 and output to local"
 base64 --decode ./key.temp > ./sign.key
 ls -l ./
-echo "====="
+
+# go back to root folder
 cd ..
-ls -l ./
